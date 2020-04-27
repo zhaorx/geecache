591d60e43703f12f60fc5e752e9ca9ac78f12fc2
zhaorx <zhaorenxiang1988@gmail.com>
2020-04-27 17:14:01 +0800

    day1 over;lru test pass
    Signed-off-by: zhaorx <zhaorenxiang1988@gmail.com>

--- lru/lru.go
+++ lru/lru.go
@@ -2,6 +2,7 @@ package lru
 
 import "container/list"
 
+// 缓存对象
 type Cache struct {
 	maxBytes int64 // 允许使用的最大内存
 	nbytes   int64 // 当前已使用的内存
@@ -11,6 +12,7 @@ type Cache struct {
 	OnEvicted func(key string, value Value) // 某条记录被移除时的回调函数
 }
 
+// 双向链表节点的数据类型
 type entry struct {
 	key   string
 	value Value
@@ -21,10 +23,59 @@ type Value interface {
 	Len() int
 }
 
-func New(maxBytes int64, OnEvicted func(string, value)) *Cache {
+// Cache Constructor
+func New(maxBytes int64, OnEvicted func(string, Value)) *Cache {
 	return &Cache{
+		cache:     make(map[string]*list.Element),
 		maxBytes:  maxBytes,
 		ll:        list.New(),
 		OnEvicted: OnEvicted,
 	}
+}
+
+// Get look ups a key's value
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// remove cache
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// Add add a element to th cache
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		kv := ele.Value.(*entry)
+		c.ll.MoveToFront(ele)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.nbytes += int64(len(key)) + int64(value.Len())
+		c.cache[key] = ele
+	}
+
+	for c.maxBytes != 0 && c.nbytes > c.maxBytes {
+		c.RemoveOldest()
+	}
+}
+
+// Len the number of cache entries
+func (c *Cache) Len() int {
+	return c.ll.Len()
 }
